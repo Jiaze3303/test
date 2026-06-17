6d547598492cdbe3130d9156e523f6954fd08fb5
Jiaze3303 <chenjiaze3.1@gmail.com>
2026-06-17 14:06:15 +0800

    Add files via upload

--- js/data/peidan.js
+++ js/data/peidan.js
@@ -1,1 +1,237 @@
+// peidan.js - 配单数据文件
+// 生成时间：2026/6/17 12:46:19
 
+window.PEIDAN_DATA = {
+  "currentConfig": {
+    "productCategory": "ID800 工业读码器",
+    "productSeries": "ID800",
+    "productModel": "MV-ID803M-03S-WBN/WBP-SR-U(线)",
+    "standardAccessories": [
+      {
+        "category": "大类",
+        "name": "U 口线缆",
+        "code": "101523961",
+        "detail": "10P10C转OPEN+USB-AM,2m,黑色线缆特性：弯折30W次，运动速度30次/分，弯折半径：12D；"
+      },
+      {
+        "category": "大类",
+        "name": "螺丝包",
+        "code": "193100773",
+        "detail": "螺丝包(3_PSF0M3×6-SUS、绿色PE袋)"
+      },
+      {
+        "category": "大类",
+        "name": "泡棉",
+        "code": "194405409",
+        "detail": "192x140x80 机器视觉 ID2000E相机 EPE"
+      },
+      {
+        "category": "大类",
+        "name": "外包装",
+        "code": "194213827",
+        "detail": "195x145x135 机器视觉 视觉传感器黄包"
+      }
+    ],
+    "optionalAccessories": [
+      {
+        "category": "其他",
+        "name": "安装板",
+        "code": "190193441",
+        "detail": "V-读码器005-安装支架M3适配ID2013EM、ID800M使用，L型，安装孔位为M6，相机孔位M3"
+      },
+      {
+        "category": "线缆",
+        "name": "串口线缆",
+        "code": "101523962",
+        "detail": "10P10C转OPEN+DB9F(DC-J),1.5m,黑色 线缆特性：弯折30W次，运动速度30次/分，弯折半径：12D；"
+      },
+      {
+        "category": "线缆",
+        "name": "组合线缆",
+        "code": "101524790",
+        "detail": "10P10C转OPEN+DB9F(DC-J)+USB-AM,2m,黑"
+      },
+      {
+        "category": "电源",
+        "name": "电源适配器",
+        "code": "310100899",
+        "detail": "电源适配器_12V2A_ADS-26FSG-12 12024EPCN,AC100-240V,国标,24W,两线,VI"
+      }
+    ]
+  },
+  "modelList": [
+    {
+      "productCategory": "ID800 工业读码器",
+      "productSeries": "ID800",
+      "productModel": "MV-ID803M-03S-WBN/WBP-SR-U(线)",
+      "standardAccessories": [
+        {
+          "category": "大类",
+          "name": "U 口线缆",
+          "code": "101523961",
+          "detail": "10P10C转OPEN+USB-AM,2m,黑色线缆特性：弯折30W次，运动速度30次/分，弯折半径：12D；"
+        },
+        {
+          "category": "大类",
+          "name": "螺丝包",
+          "code": "193100773",
+          "detail": "螺丝包(3_PSF0M3×6-SUS、绿色PE袋)"
+        },
+        {
+          "category": "大类",
+          "name": "泡棉",
+          "code": "194405409",
+          "detail": "192x140x80 机器视觉 ID2000E相机 EPE"
+        },
+        {
+          "category": "大类",
+          "name": "外包装",
+          "code": "194213827",
+          "detail": "195x145x135 机器视觉 视觉传感器黄包"
+        }
+      ],
+      "optionalAccessories": [
+        {
+          "category": "其他",
+          "name": "安装板",
+          "code": "190193441",
+          "detail": "V-读码器005-安装支架M3适配ID2013EM、ID800M使用，L型，安装孔位为M6，相机孔位M3"
+        },
+        {
+          "category": "线缆",
+          "name": "串口线缆",
+          "code": "101523962",
+          "detail": "10P10C转OPEN+DB9F(DC-J),1.5m,黑色 线缆特性：弯折30W次，运动速度30次/分，弯折半径：12D；"
+        },
+        {
+          "category": "线缆",
+          "name": "组合线缆",
+          "code": "101524790",
+          "detail": "10P10C转OPEN+DB9F(DC-J)+USB-AM,2m,黑"
+        },
+        {
+          "category": "电源",
+          "name": "电源适配器",
+          "code": "310100899",
+          "detail": "电源适配器_12V2A_ADS-26FSG-12 12024EPCN,AC100-240V,国标,24W,两线,VI"
+        }
+      ]
+    },
+    {
+      "productCategory": "ID800 工业读码器",
+      "productSeries": "ID8000",
+      "productModel": "MV-ID803M-03S-WBN/WBP-SR-U(基线)",
+      "standardAccessories": [
+        {
+          "category": "",
+          "name": "U 口线缆",
+          "code": "101523961",
+          "detail": "10P10C转OPEN+USB-AM,2m,黑色线缆特性：弯折30W次，运动速度30次/分，弯折半径：12D；"
+        },
+        {
+          "category": "",
+          "name": "螺丝包",
+          "code": "193100773",
+          "detail": "螺丝包(3_PSF0M3×6-SUS、绿色PE袋)"
+        },
+        {
+          "category": "",
+          "name": "泡棉",
+          "code": "194405409",
+          "detail": "192x140x80 机器视觉 ID2000E相机 EPE"
+        },
+        {
+          "category": "",
+          "name": "外包装",
+          "code": "194213827",
+          "detail": "195x145x135 机器视觉 视觉传感器黄包"
+        }
+      ],
+      "optionalAccessories": [
+        {
+          "category": "",
+          "name": "安装板",
+          "code": "190193441",
+          "detail": "V-读码器005-安装支架M3适配ID2013EM、ID800M使用，L型，安装孔位为M6，相机孔位M3"
+        },
+        {
+          "category": "",
+          "name": "串口线缆",
+          "code": "101523962",
+          "detail": "10P10C转OPEN+DB9F(DC-J),1.5m,黑色 线缆特性：弯折30W次，运动速度30次/分，弯折半径：12D；"
+        },
+        {
+          "category": "",
+          "name": "组合线缆",
+          "code": "101524790",
+          "detail": "10P10C转OPEN+DB9F(DC-J)+USB-AM,2m,黑"
+        },
+        {
+          "category": "",
+          "name": "电源适配器",
+          "code": "310100899",
+          "detail": "电源适配器_12V2A_ADS-26FSG-12 12024EPCN,AC100-240V,国标,24W,两线,VI"
+        }
+      ]
+    },
+    {
+      "productCategory": "ID8000 工业读码器",
+      "productSeries": "ID800",
+      "productModel": "MV-ID803M-03S-WBN/WBP-SR-U(基线)",
+      "standardAccessories": [
+        {
+          "category": "",
+          "name": "U 口线缆",
+          "code": "101523961",
+          "detail": "10P10C转OPEN+USB-AM,2m,黑色线缆特性：弯折30W次，运动速度30次/分，弯折半径：12D；"
+        },
+        {
+          "category": "",
+          "name": "螺丝包",
+          "code": "193100773",
+          "detail": "螺丝包(3_PSF0M3×6-SUS、绿色PE袋)"
+        },
+        {
+          "category": "",
+          "name": "泡棉",
+          "code": "194405409",
+          "detail": "192x140x80 机器视觉 ID2000E相机 EPE"
+        },
+        {
+          "category": "",
+          "name": "外包装",
+          "code": "194213827",
+          "detail": "195x145x135 机器视觉 视觉传感器黄包"
+        }
+      ],
+      "optionalAccessories": [
+        {
+          "category": "",
+          "name": "安装板",
+          "code": "190193441",
+          "detail": "V-读码器005-安装支架M3适配ID2013EM、ID800M使用，L型，安装孔位为M6，相机孔位M3"
+        },
+        {
+          "category": "",
+          "name": "串口线缆",
+          "code": "101523962",
+          "detail": "10P10C转OPEN+DB9F(DC-J),1.5m,黑色 线缆特性：弯折30W次，运动速度30次/分，弯折半径：12D；"
+        },
+        {
+          "category": "",
+          "name": "组合线缆",
+          "code": "101524790",
+          "detail": "10P10C转OPEN+DB9F(DC-J)+USB-AM,2m,黑"
+        },
+        {
+          "category": "",
+          "name": "电源适配器",
+          "code": "310100899",
+          "detail": "电源适配器_12V2A_ADS-26FSG-12 12024EPCN,AC100-240V,国标,24W,两线,VI"
+        }
+      ]
+    }
+  ]
+};
+
+if (window.BOM && window.BOM.applyData) { window.BOM.applyData(window.PEIDAN_DATA); }
