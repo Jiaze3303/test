35ab36b2c128d3f5435fbc555b637fff67e43127
Jiaze3303 <chenjiaze3.1@gmail.com>
2026-06-18 10:37:01 +0800

    Add files via upload

--- js/data/peidan.js
+++ js/data/peidan.js
@@ -1,10 +1,10 @@
 // peidan.js - 配单数据文件
-// 生成时间：2026/6/17 12:46:19
+// 生成时间：2026/6/18 10:17:21
 
 window.PEIDAN_DATA = {
   "currentConfig": {
     "productCategory": "ID800 工业读码器",
-    "productSeries": "ID800",
+    "productSeries": "ID830",
     "productModel": "MV-ID803M-03S-WBN/WBP-SR-U(线)",
     "standardAccessories": [
       {
@@ -56,6 +56,36 @@ window.PEIDAN_DATA = {
         "name": "电源适配器",
         "code": "310100899",
         "detail": "电源适配器_12V2A_ADS-26FSG-12 12024EPCN,AC100-240V,国标,24W,两线,VI"
+      },
+      {
+        "category": "2",
+        "name": "2",
+        "code": "代码",
+        "detail": "备注"
+      },
+      {
+        "category": "21",
+        "name": "3",
+        "code": "代码",
+        "detail": "备注"
+      },
+      {
+        "category": "56",
+        "name": "34",
+        "code": "代码",
+        "detail": "备注"
+      },
+      {
+        "category": "11",
+        "name": "23",
+        "code": "代码",
+        "detail": "备注"
+      },
+      {
+        "category": "45",
+        "name": "23",
+        "code": "代码",
+        "detail": "备注"
       }
     ]
   },
@@ -228,6 +258,93 @@ window.PEIDAN_DATA = {
           "name": "电源适配器",
           "code": "310100899",
           "detail": "电源适配器_12V2A_ADS-26FSG-12 12024EPCN,AC100-240V,国标,24W,两线,VI"
+        }
+      ]
+    },
+    {
+      "productCategory": "ID800 工业读码器",
+      "productSeries": "ID830",
+      "productModel": "MV-ID803M-03S-WBN/WBP-SR-U(线)",
+      "standardAccessories": [
+        {
+          "category": "大类",
+          "name": "U 口线缆",
+          "code": "101523961",
+          "detail": "10P10C转OPEN+USB-AM,2m,黑色线缆特性：弯折30W次，运动速度30次/分，弯折半径：12D；"
+        },
+        {
+          "category": "大类",
+          "name": "螺丝包",
+          "code": "193100773",
+          "detail": "螺丝包(3_PSF0M3×6-SUS、绿色PE袋)"
+        },
+        {
+          "category": "大类",
+          "name": "泡棉",
+          "code": "194405409",
+          "detail": "192x140x80 机器视觉 ID2000E相机 EPE"
+        },
+        {
+          "category": "大类",
+          "name": "外包装",
+          "code": "194213827",
+          "detail": "195x145x135 机器视觉 视觉传感器黄包"
+        }
+      ],
+      "optionalAccessories": [
+        {
+          "category": "其他",
+          "name": "安装板",
+          "code": "190193441",
+          "detail": "V-读码器005-安装支架M3适配ID2013EM、ID800M使用，L型，安装孔位为M6，相机孔位M3"
+        },
+        {
+          "category": "线缆",
+          "name": "串口线缆",
+          "code": "101523962",
+          "detail": "10P10C转OPEN+DB9F(DC-J),1.5m,黑色 线缆特性：弯折30W次，运动速度30次/分，弯折半径：12D；"
+        },
+        {
+          "category": "线缆",
+          "name": "组合线缆",
+          "code": "101524790",
+          "detail": "10P10C转OPEN+DB9F(DC-J)+USB-AM,2m,黑"
+        },
+        {
+          "category": "电源",
+          "name": "电源适配器",
+          "code": "310100899",
+          "detail": "电源适配器_12V2A_ADS-26FSG-12 12024EPCN,AC100-240V,国标,24W,两线,VI"
+        },
+        {
+          "category": "2",
+          "name": "2",
+          "code": "代码",
+          "detail": "备注"
+        },
+        {
+          "category": "21",
+          "name": "3",
+          "code": "代码",
+          "detail": "备注"
+        },
+        {
+          "category": "56",
+          "name": "34",
+          "code": "代码",
+          "detail": "备注"
+        },
+        {
+          "category": "11",
+          "name": "23",
+          "code": "代码",
+          "detail": "备注"
+        },
+        {
+          "category": "45",
+          "name": "23",
+          "code": "代码",
+          "detail": "备注"
         }
       ]
     }
